2fcb638cda19234c0aeb07c90ad400ea15a06b02
MatiasPhoenix <matias.la19@gmail.com>
2024-05-23 15:04:55 +0200

    header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { HeaderComponent } from './Components/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: "header", component: HeaderComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
