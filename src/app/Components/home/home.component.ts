09ebfd20979c659b50da32cccf689d85b1a08a46
MatiasPhoenix <matias.la19@gmail.com>
2024-05-24 10:42:19 +0200

    modificata struttura/aggiunti png

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -13,10 +13,34 @@ export class HomeComponent {
   constructor (private localStoreService: LocalStoreService){}
 
   toDoItem     : string      = "";
+  postIt       : number[]    = [0,1,2,3];
   toDoList     : ToDoList[]  = [];
 
   ngOnInit(){
     this.toDoList = this.localStoreService.getToDoList('todos');
+  }
+
+  postItMetod(){
+    let numPostIt: number = Math.floor(Math.random() * 4) + 1;
+    switch (numPostIt) {
+      case 1:
+      return '../../../assets/post-it-01.png'
+
+      case 2:
+      return '../../../assets/post-it-02.png'
+
+      case 3:
+      return '../../../assets/post-it-03.png'
+
+      case 4:
+      return '../../../assets/post-it-04.png'
+
+      default:
+        console.log('non ci sono immagini');
+        return ''
+
+    }
+
   }
 
   addToDo(){
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
-import { HeaderComponent } from './Components/header/header.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: "header", component: HeaderComponent}
+
 ];
 
 @NgModule({
